6fb95defa258ad6bba0fa5a46b699172ca0274c0
Shunnyyx <cattomeowuwu@gmail.com>
2024-09-24 12:51:09 -0500

    Añadido endpoint para GIFs de abrazos de anime

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const CustomEmbedBuilder = require('./functions/embedBuilder');
-const commandFunctions = require('./functions/commandFunctions'); // Importa todos los métodos de funciones
+const commandFunctions = require('./functions/commandFunctions');
 
 class Toru {
   constructor(options) {
@@ -26,8 +26,7 @@ class Toru {
     });
 
     this.client.on('messageCreate', (message) => {
-      if (message.author.bot) return;
-      if (!message.content.startsWith(this.prefix)) return;
+      if (message.author.bot || !message.content.startsWith(this.prefix)) return;
       this.handleMessage(message);
     });
   }
@@ -50,18 +49,7 @@ class Toru {
 
     if (command) {
       if (command.embed) {
-        const embedBuilder = new CustomEmbedBuilder()
-          .addTitle(this.replacePlaceholders(command.embed.title, message))
-          .addDescription(this.replacePlaceholders(command.embed.description, message))
-          .addFooter(
-            this.replacePlaceholders(command.embed.footer?.text || '', message),
-            command.embed.footer?.iconURL ? this.replacePlaceholders(command.embed.footer.iconURL, message) : null
-          )
-          .addColor(command.embed.color || '#FFFFFF')
-          .addImage(this.replacePlaceholders(command.embed.image || '', message))
-          .addThumbnail(this.replacePlaceholders(command.embed.thumbnail || '', message));
-
-        message.channel.send({ embeds: [embedBuilder.build()] });
+        this.handleEmbedCommand(command, message);
       } else if (command.response) {
         const response = this.replacePlaceholders(command.response, message);
         message.channel.send(response);
@@ -74,46 +62,59 @@ class Toru {
     }
   }
 
+  handleEmbedCommand(command, message) {
+    const embedBuilder = new CustomEmbedBuilder()
+      .addTitle(this.replacePlaceholders(command.embed.title, message))
+      .addDescription(this.replacePlaceholders(command.embed.description, message))
+      .addFooter(
+        this.replacePlaceholders(command.embed.footer?.text || '', message),
+        command.embed.footer?.iconURL ? this.replacePlaceholders(command.embed.footer.iconURL, message) : null
+      )
+      .addColor(command.embed.color || '#FFFFFF')
+      .addImage(this.replacePlaceholders(command.embed.image || '', message))
+      .addThumbnail(this.replacePlaceholders(command.embed.thumbnail || '', message));
+
+    message.channel.send({ embeds: [embedBuilder.build()] });
+  }
+
   replacePlaceholders(text, message) {
     const iconURL = message.guild.iconURL({ format: 'png', size: 1024 });
 
     return text
-      .replace('<$client.ping>', commandFunctions.clientPing(this.client))
-      .replace('<$message.ping>', commandFunctions.messagePing(message))
-      .replace('<$client.username>', commandFunctions.clientUsername(this.client))
-      .replace('<$author.username>', commandFunctions.authorUsername(message))
-      .replace('<$user.id>', commandFunctions.userId(message.author))
-      .replace('<$guild.name>', commandFunctions.guildName(message.guild))
-      .replace('<$channel.name>', commandFunctions.channelName(message.channel))
-      .replace('<$guild.memberCount>', commandFunctions.guildMemberCount(message.guild))
-      .replace('<$guild.icon>', iconURL || '')
-      .replace('<$channel.id>', message.channel.id)
-      .replace('<$message.createdAt>', commandFunctions.messageCreatedAt(message))
-      .replace('<$user.tag>', commandFunctions.userTag(message.author))
-      .replace('<$member.joinedAt>', commandFunctions.memberJoinedAt(message.member))
-      .replace('<$guild.region>', commandFunctions.guildRegion(message.guild))
-      .replace('<$client.uptime>', commandFunctions.clientUptime(this.client))
-      .replace('<$message.content>', commandFunctions.messageContent(message))
+      .replaceAll('<$client.ping>', commandFunctions.clientPing(this.client))
+      .replaceAll('<$message.ping>', commandFunctions.messagePing(message))
+      .replaceAll('<$client.username>', commandFunctions.clientUsername(this.client))
+      .replaceAll('<$author.username>', commandFunctions.authorUsername(message))
+      .replaceAll('<$user.id>', commandFunctions.userId(message.author))
+      .replaceAll('<$guild.name>', commandFunctions.guildName(message.guild))
+      .replaceAll('<$channel.name>', commandFunctions.channelName(message.channel))
+      .replaceAll('<$guild.memberCount>', commandFunctions.guildMemberCount(message.guild))
+      .replaceAll('<$guild.icon>', iconURL || '')
+      .replaceAll('<$channel.id>', message.channel.id)
+      .replaceAll('<$message.createdAt>', commandFunctions.messageCreatedAt(message))
+      .replaceAll('<$user.tag>', commandFunctions.userTag(message.author))
+      .replaceAll('<$member.joinedAt>', commandFunctions.memberJoinedAt(message.member))
+      .replaceAll('<$guild.region>', commandFunctions.guildRegion(message.guild))
+      .replaceAll('<$client.uptime>', commandFunctions.clientUptime(this.client))
+      .replaceAll('<$message.content>', commandFunctions.messageContent(message))
       .replace(/<\$channel.type(?:\: "(.*?)")?>/g, (match, id) => {
-        if (id) {
-          return commandFunctions.channelType(message.guild, id);
-        } else {
-          return commandFunctions.channelType(message.channel);
-        }
+        return id ? commandFunctions.channelType(message.guild, id) : commandFunctions.channelType(message.channel);
       })
-      .replace(/<\$image: "(.*?)">/g, (match, url) => commandFunctions.image(url))
-      .replace(/<\$thumbnail: "(.*?)">/g, (match, url) => {
-        try {
-          new URL(url);
-          return url;
-        } catch (e) {
-          console.error(`URL de thumbnail no válida: ${url}`);
-          return '';
-        }
-      })
-      .replace('<$client.guildCount>', commandFunctions.clientGuildCount(this.client))
-      .replace('<$client.memberCount>', commandFunctions.clientMemberCount(this.client))
-      .replace('<$commands.count>', commandFunctions.commandsCount(this.commands));
+      .replace(/<\$image: "(.*?)">/g, (_, url) => commandFunctions.image(url))
+      .replace(/<\$thumbnail: "(.*?)">/g, (_, url) => this.validateURL(url) ? url : '')
+      .replaceAll('<$client.guildCount>', commandFunctions.clientGuildCount(this.client))
+      .replaceAll('<$client.memberCount>', commandFunctions.clientMemberCount(this.client))
+      .replaceAll('<$commands.count>', commandFunctions.commandsCount(this.commands));
+  }
+
+  validateURL(url) {
+    try {
+      new URL(url);
+      return true;
+    } catch {
+      console.error(`URL no válida: ${url}`);
+      return false;
+    }
   }
 
   loadCommands(directory) {
